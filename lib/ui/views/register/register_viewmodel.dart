18d4b4a0d95aa61815afb4c9a889c0afa44476bf
Muhammad Sulthan Mazaya <73002733+MSMazaya@users.noreply.github.com>
2023-05-22 21:03:36 +0700

    return working register view and viewmodel after missing in conflict

--- lib/ui/views/register/register_viewmodel.dart
+++ lib/ui/views/register/register_viewmodel.dart
@@ -1,11 +1,15 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:transitspot/app/app.locator.dart';
+import 'package:transitspot/app/app.router.dart';
 import 'package:transitspot/ui/views/register/register_view.form.dart';
 
 class RegisterViewModel extends FormViewModel {
-  final String _title = 'Home View';
+  final String _title = 'Register View';
   String get title => _title;
   bool _isSignUp = true;
   bool get isSignUp => _isSignUp;
+  final _navigationService = locator<NavigationService>();
 
   void setIsSignUp(bool state) {
     _isSignUp = state;
@@ -15,5 +19,9 @@ class RegisterViewModel extends FormViewModel {
   @override
   void setFormStatus() {
     // TODO: implement setFormStatus
+  }
+
+  Future toHome() async {
+    await _navigationService.navigateTo(Routes.homeView);
   }
 }
