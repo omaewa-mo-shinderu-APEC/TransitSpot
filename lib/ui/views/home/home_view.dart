ad2b642f6924b84ef6ff012f6fca275248cc1e72
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2023-05-22 16:04:03 +0700

    feat: add home page

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:transitspot/ui/views/home/home_viewmodel.dart';
 import 'package:transitspot/utils/app_color.dart';
-import 'package:transitspot/utils/app_text.dart';
+import 'package:transitspot/ui/views/home/component/user_card.dart';
+import 'package:transitspot/ui/views/home/component/service_card.dart';
+import 'package:transitspot/ui/layout/has_login_view.dart';
 import 'package:stacked/stacked.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,23 +11,56 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<RegisterViewModel>.reactive(
+    return ViewModelBuilder<HomeViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         backgroundColor: AppColors.primaryBackground,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
         drawer: Drawer(),
-        body: Container(
-          child: AppText(
-            text: model.title,
-            color: AppColors.content,
-            size: 20,
+        body: HasLoginView(
+          childWidget: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    model.title1,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.content,
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  UserCard(
+                    uname: model.uname,
+                    role: model.role,
+                    // TODO: add image
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    model.title2,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.content,
+                      fontSize: 24,
+                    ),
+                  ),
+                  ServiceCard(
+                    title: model.srvs[0]['title']!,
+                    iconSrv: model.srvs[0]['img']!,
+                  ),
+                  ServiceCard(
+                    title: model.srvs[1]['title']!,
+                    iconSrv: model.srvs[1]['img']!,
+                  )
+                  // Text('halo')
+                ],
+              ),
+            ),
           ),
         ),
       ),
-      viewModelBuilder: () => RegisterViewModel(),
+      viewModelBuilder: () => HomeViewModel(),
     );
   }
 }
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,6 +1,36 @@
 import 'package:stacked/stacked.dart';
 
-class RegisterViewModel extends BaseViewModel {
-  final String _title = 'Home View';
-  String get title => _title;
+class HomeViewModel extends BaseViewModel {
+  final String _title1 = 'Welcome back';
+  final String _title2 = 'Services';
+  final String _uname = "Stephen Strange";
+  final String _role = "Customer";
+  final List<Map<String, String>> _srvCustomer = [
+    {
+      'title': "Book a Seat",
+      'img': 'lib/assets/home/promo.png',
+    },
+    {
+      'title': "Find on Demand",
+      'img': 'lib/assets/home/distance.png',
+    },
+  ];
+  final List<Map<String, String>> _srvDriver = [
+    {
+      'title': "Activate Services",
+      'img': 'lib/assets/home/current_location.png',
+    },
+    {
+      'title': "Open Map",
+      'img': 'lib/assets/home/distance.png',
+    },
+  ];
+
+  String get image => 'lib/assets/home/dummy-avatar.png';
+  String get title1 => _title1;
+  String get title2 => _title2;
+  String get uname => _uname;
+  String get role => _role;
+  List<Map<String, String>> get srvs =>
+      (_role == 'Customer') ? _srvCustomer : _srvDriver;
 }
